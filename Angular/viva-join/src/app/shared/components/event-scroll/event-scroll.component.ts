3d04e3d6ed4675585d0d288997c70b36072f041e
Carlos Dolz Martin <carlos.dolz@fain.local>
2024-03-12 16:52:38 +0100

    master: swiper properties TODO

--- Angular/viva-join/src/app/shared/components/event-scroll/event-scroll.component.ts
+++ Angular/viva-join/src/app/shared/components/event-scroll/event-scroll.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { register } from 'swiper/element/bundle';
+register();
+
 @Component({
   selector: 'app-shared-event-scroll',
   templateUrl: './event-scroll.component.html',
   styleUrl: './event-scroll.component.scss'
 })
 export class EventScrollComponent {
+
   public events: string [] = ['evento','evento','evento','evento','evento','evento','evento','evento','evento','evento'];
 }
--- Angular/viva-join/src/app/shared/shared.module.ts
+++ Angular/viva-join/src/app/shared/shared.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 import { CommonModule } from '@angular/common';
 import { EventScrollComponent } from './components/event-scroll/event-scroll.component';
 
-
 @NgModule({    
     declarations: [LoginFormComponent, SignUpFormComponent, EventScrollComponent],
     exports:[LoginFormComponent, SignUpFormComponent, EventScrollComponent],
-    imports:[CommonModule]
+    imports:[CommonModule],
+    schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule { }
