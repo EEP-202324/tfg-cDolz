85ea9b98218ea61312fac00f2208866daff3d0d0
Carlos Dolz Martin <carlos.dolz@fain.local>
2024-03-15 13:51:24 +0100

    master: swiper, implementing methods, implementing own css styles

--- Angular/viva-join/src/app/shared/components/event-scroll/event-scroll.component.ts
+++ Angular/viva-join/src/app/shared/components/event-scroll/event-scroll.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import swiper from 'swiper';
+import SwiperCore, { Navigation, Swiper } from 'swiper';
+
+SwiperCore.use([Navigation]);
 
 @Component({
   selector: 'app-shared-event-scroll',
   templateUrl: './event-scroll.component.html',
   styleUrl: './event-scroll.component.scss'
 })
-export class EventScrollComponent {  
+export class EventScrollComponent{  
     
   public events: string[] = ['evento1', 'evento2', 'evento3', 'evento4', 'evento5', 'evento6', 'evento7', 'evento8', 'evento9', 'evento10'];
 
 }
-
