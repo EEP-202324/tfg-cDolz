9889a1b36707ada7719352fd70e32c4d463feaae
Carlos Dolz Martin <carlos.dolz@fain.local>
2024-03-18 16:54:28 +0100

    master: opacity arrow slider, add dark theme when hover

--- Angular/viva-join/src/app/shared/components/event-scroll/event-scroll.component.ts
+++ Angular/viva-join/src/app/shared/components/event-scroll/event-scroll.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import swiper from 'swiper';
+import { Component, ElementRef } from '@angular/core';
 import SwiperCore, { Navigation, Swiper } from 'swiper';
 
 SwiperCore.use([Navigation]);
@@ -9,8 +8,9 @@ SwiperCore.use([Navigation]);
   templateUrl: './event-scroll.component.html',
   styleUrl: './event-scroll.component.scss'
 })
-export class EventScrollComponent{  
-    
-  public events: string[] = ['evento1', 'evento2', 'evento3', 'evento4', 'evento5', 'evento6', 'evento7', 'evento8', 'evento9', 'evento10'];
+export class EventScrollComponent {  
 
+  public events: string[] = ['evento1', 'evento2', 'evento3', 'evento4', 'evento5', 'evento6', 'evento7', 'evento8', 'evento9',
+    'evento10', 'evento11', 'evento12', 'evento13', 'evento14', 'evento15']; 
+  
 }
