45e94f03c762d643da5a4ff80603f16eb3694550
Carlos Dolz Martin <carlos.dolz@fain.local>
2024-03-21 16:49:49 +0100

    master: changing slide to ts events, almost works

--- Angular/viva-join/src/app/shared/components/event-scroll/event-scroll.component.ts
+++ Angular/viva-join/src/app/shared/components/event-scroll/event-scroll.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef } from '@angular/core';
-import SwiperCore, { Navigation, Swiper } from 'swiper';
+import { AfterViewInit, Component, ElementRef, QueryList, TemplateRef, ViewChild, ViewChildren } from '@angular/core';
+import SwiperCore, { Navigation } from 'swiper';
 
 SwiperCore.use([Navigation]);
 
@@ -8,9 +8,43 @@ SwiperCore.use([Navigation]);
   templateUrl: './event-scroll.component.html',
   styleUrl: './event-scroll.component.scss'
 })
-export class EventScrollComponent {  
+export class EventScrollComponent {
 
   public events: string[] = ['evento1', 'evento2', 'evento3', 'evento4', 'evento5', 'evento6', 'evento7', 'evento8', 'evento9',
-    'evento10', 'evento11', 'evento12', 'evento13', 'evento14', 'evento15']; 
-  
+    'evento10', 'evento11', 'evento12', 'evento13', 'evento14', 'evento15'];
+
+  @ViewChildren('divSwiper') previousSwiperList!: QueryList<ElementRef>;
+
+  @ViewChildren('slideImg') previousImgList!: QueryList<ElementRef>;
+
+  movePreviousSliders(actualIndex: number) {    
+    this.previousSwiperList.forEach((element: ElementRef, index: number) => {
+      if(index < actualIndex){
+        element.nativeElement.style.transform = 'translateX(-9%)';        
+      }
+      if(index > actualIndex){
+        element.nativeElement.style.transform = 'translateX(9%)';        
+      }
+    });
+    this.previousImgList.forEach((element: ElementRef, index: number) => {
+      if(index != actualIndex){
+        element.nativeElement.style.opacity = 0.5;        
+      }
+    });
+  }
+
+  resetPreviousSliders(actualIndex: number) {
+    console.log(actualIndex);
+    this.previousSwiperList.forEach((element: ElementRef, index: number) => {
+      if(index != actualIndex){
+        element.nativeElement.style.transform = 'translateX(0%)';
+      }
+    });
+    this.previousImgList.forEach((element: ElementRef, index: number) => {
+      if(index != actualIndex){
+        element.nativeElement.style.opacity = 1;
+      } 
+    });
+  }
+
 }
