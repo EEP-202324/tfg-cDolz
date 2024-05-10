264467c0f6164d5b2ff1b1462810c3b764060789
cDolz <carlosdolzm@gmail.com>
2024-05-10 16:35:58 +0200

    master: clean commit

--- Angular/viva-join/src/environments/environments.ts
+++
@@ -1,6 +0,0 @@
-import { Environment } from "../app/models/environment.model";
-
-export const environment: Environment = {    
-    // url base de las peticiones al back
-    baseUrl: 'http://localhost:3000/api'
-};
